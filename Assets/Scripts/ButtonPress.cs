6ccd4d279d3d44bbcfcbf8a3ff04e5664eef35cd
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-03-23 10:32:24 +0100

    calculator body

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    [SerializeField] int btnNbr;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
